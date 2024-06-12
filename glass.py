2366e2ca1d1e734b7a31a88a55c85f94c6e8dece
Paul Cobbaut <paul@cobbaut.be>
2024-06-12 11:38:44 +0200

    adding the three holes

--- glass.py
+++ glass.py
@@ -1,11 +1,12 @@
 """
-glass.py -- Paul Cobbaut, 2024-05-18
+glass.py -- Paul Cobbaut
+2024-05-18
 2024-05-27
 2024-06-02
 2024-06-12
 Create a hexagon wall display for small figurines.
 3D-printed hexagon, covered by a bought plexiglass panel.
-This file ==> svg and dxf for plexiglass panel
+This file ==> create svg and dxf for plexiglass panel
 """
 
 import FreeCAD
@@ -15,14 +16,17 @@ import Sketcher
 from ProfileLib import RegularPolygon 
 import math
 import importSVG
+import importDXF
 
 # Dimensions in mm
-hexa = 6
+hexa           =   6
 outer_radius   = 166
 corner_radius  =  20
 rounder_radius = outer_radius - corner_radius
 hingecut       =  10
 hingewidth     =  64
+holeradius     =   1.50
+hingeholedist  =  20
 
 # labels
 DocLabel    = 'Hexagon Glass Panel'
@@ -35,9 +39,6 @@ doc    = FreeCAD.newDocument(DocLabel)
 body   = doc.addObject("PartDesign::Body", BodyLabel)
 sketch = doc.getObject(BodyLabel).newObject("Sketcher::SketchObject", SketchLabel)
 sketch.Placement = FreeCAD.Placement(Vector(0,0,0),FreeCAD.Rotation(Vector(1,0,0),0))
-
-# calculate the points needed
-# six rounded corners = six center points + twelve
 
 # rounded corners
 # calculate the six centers of the arcs that serve as corners
@@ -73,7 +74,6 @@ sketch.addGeometry(Part.ArcOfCircle(Part.Circle(center5, axis, corner_radius), s
 startangle = startangle + math.radians(60)
 endangle   = endangle   + math.radians(60)
 sketch.addGeometry(Part.ArcOfCircle(Part.Circle(center6, axis, corner_radius), startangle, endangle),False)
-
 doc.recompute()
 
 # calculate the points for the top gap for the hinge
@@ -107,11 +107,10 @@ e = sketch.Shape.Edges[6]
 startpoint6 = e.Vertexes[0].Point
 #endpoint1   = e.Vertexes[1].Point
 
-
 # draw the line pieces between the end points of the arcs
 line1 = sketch.addGeometry(Part.LineSegment(startpoint1, endpoint1),False)
 line2a = sketch.addGeometry(Part.LineSegment(startpoint2, leftpoint),False)
-lineh1 = sketch.addGeometry(Part.LineSegment(rightpoint    , rightpointdown), False)
+lineh1 = sketch.addGeometry(Part.LineSegment(rightpoint    , rightpointdown), False) # hinge
 lineh2 = sketch.addGeometry(Part.LineSegment(rightpointdown, leftpointdown ), False)
 lineh3 = sketch.addGeometry(Part.LineSegment(leftpointdown , leftpoint     ), False)
 line2b = sketch.addGeometry(Part.LineSegment(rightpoint, endpoint2),False)
@@ -120,8 +119,7 @@ line4 = sketch.addGeometry(Part.LineSegment(startpoint4, endpoint4),False)
 line5 = sketch.addGeometry(Part.LineSegment(startpoint5, endpoint5),False)
 line6 = sketch.addGeometry(Part.LineSegment(startpoint6, endpoint6),False)
 
-
-#doc.removeObject('rounder')
+# remove obsolete lines
 sketch.delGeometries([0])
 sketch.delGeometries([0])
 sketch.delGeometries([0])
@@ -129,15 +127,22 @@ sketch.delGeometries([0])
 sketch.delGeometries([0])
 sketch.delGeometries([0])
 sketch.delGeometries([0])
-
 doc.recompute()
 
+# draw holes, two at the top, one at the bottom
+hole1center = Vector( hingeholedist/2,  leftpoint.y - 10, 0) # leftpoint.y == rightpoint.y
+hole2center = Vector(-hingeholedist/2,  leftpoint.y - 10, 0)
+hole3center = Vector(0               , -leftpoint.y + 10, 0)
+sketch.addGeometry(Part.Circle(hole1center,axis,holeradius),False)
+sketch.addGeometry(Part.Circle(hole2center,axis,holeradius),False)
+sketch.addGeometry(Part.Circle(hole3center,axis,holeradius),False)
+doc.recompute()
 
+# export SVG and DXF
 objs = []
 objs.append(doc.getObject("Glass_Sketch"))
 importSVG.export(objs, u"/home/paul/FreeCAD models/smurf/Hexagon Glass sketch.svg")
+importDXF.export(objs, u"/home/paul/FreeCAD models/smurf/Hexagon Glass sketch.dxf")
 
 
-
-doc.recompute()
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
