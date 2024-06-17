c5f5b6f1dcdc2893640108254acc1c18e5aa751b
Paul Cobbaut <paul@cobbaut.be>
2024-06-17 11:04:14 +0200

    working on shelves, sides, and corners

--- glue_two_way_corner.py
+++ glue_two_way_corner.py
@@ -1,5 +1,7 @@
 """
-zeshoek.py -- Paul Cobbaut, 2024-05-06
+Two-way Glue corner -- Paul Cobbaut
+2024-05-06
+2024-06-17
 Create a hexagon wall display for small figurines.
 3D-printed hexagon, covered by a bought panel.
 Corners for the hexagon and for the shelves.
@@ -24,23 +26,20 @@ export_directory = "/home/paul/FreeCAD_generated/hexagon/"
 
 #Dimensions in mm
 center_radius   =  5
-arm_length      = 40
+arm_length      = 20
 arm_width       =  6
 hole_width      =  2
 cover_width     = arm_width/2 - hole_width/2
 depth           = 21
-gluepart_depth  =  2
+gluepart_depth  =  1.90
 gluepart_radius = 12
 
 # Arms start as rectangles named a, b, c
 # a goes down 270 degrees
 # b goes up-right 30 degrees
-# c goes up-left 150 degrees
 
 a_deg = -90
 b_deg = a_deg + 120
-c_deg = b_deg + 120
-
 
 # functions
 #
@@ -52,9 +51,7 @@ def polar_to_vector(radius, angle_degrees):
     y = round(radius * math.sin(angle_radians),2)
     return Vector(x, y, 0)
 
-
-def circle_line_segment_intersection(circle_center, circle_radius, line_start, line_end):
-    cx, cy = circle_center
+def circle_line_segment_intersection(circle_radius, line_start, line_end):
     x1, y1 = line_start
     x2, y2 = line_end
 
@@ -62,20 +59,16 @@ def circle_line_segment_intersection(circle_center, circle_radius, line_start, l
     dy = y2 - y1
     dr = math.sqrt(dx**2 + dy**2)
     D = x1*y2 - x2*y1
-
     discriminant = circle_radius**2 * dr**2 - D**2
-
-    # Two intersection points
     sqrt_discriminant = math.sqrt(discriminant)
-    x1 = (D * dy + math.copysign(dx * sqrt_discriminant, dy)) / (dr**2)
-    y1 = (-D * dx + math.fabs(dy) * sqrt_discriminant) / (dr**2)
-    x2 = (D * dy - math.copysign(dx * sqrt_discriminant, dy)) / (dr**2)
-    y2 = (-D * dx - math.fabs(dy) * sqrt_discriminant) / (dr**2)
+    x1 = ( D * dy + math.copysign(dx * sqrt_discriminant, dy)) / (dr**2)
+    x2 = ( D * dy - math.copysign(dx * sqrt_discriminant, dy)) / (dr**2)
+    y1 = (-D * dx + math.copysign(dy * sqrt_discriminant, dx)) / (dr**2)
+    y2 = (-D * dx - math.copysign(dy * sqrt_discriminant, dx)) / (dr**2)
 
-    intersection_points = [(round(x1 + cx,1), round(y1 + cy,1)), (round(x2 + cx,1), round(y2 + cy,1))]
+    intersection_points = [(round(x1,2), round(y1,2)), (round(x2,2), round(y2,2))]
     valid_intersection_points = [point for point in intersection_points if is_point_on_line_segment(point, line_start, line_end)]
     return valid_intersection_points
-
 
 def is_point_on_line_segment(point, line_start, line_end):
     x, y = point
@@ -105,71 +98,53 @@ Circle_obj = Sketch_obj.addGeometry(Part.Circle(centerpoint, direction, radius),
 # mid points of the end lines
 am = polar_to_vector(arm_length, a_deg)
 bm = polar_to_vector(arm_length, b_deg)
-cm = polar_to_vector(arm_length, c_deg)
 
 # half points of the end lines
 a_end_half = polar_to_vector(arm_width/2, a_deg + 90) 
 b_end_half = polar_to_vector(arm_width/2, b_deg + 90) 
-c_end_half = polar_to_vector(arm_width/2, c_deg + 90) 
 
 # end points of the three arms
 a_end_for = am + a_end_half
 a_end_bac = am - a_end_half
 b_end_for = bm + b_end_half
 b_end_bac = bm - b_end_half
-c_end_for = cm + c_end_half
-c_end_bac = cm - c_end_half
 
 # end lines = short lines furthest away from origin
 a_end_line = Sketch_obj.addGeometry(Part.LineSegment(a_end_for, a_end_bac),False)
 b_end_line = Sketch_obj.addGeometry(Part.LineSegment(b_end_for, b_end_bac),False)
-c_end_line = Sketch_obj.addGeometry(Part.LineSegment(c_end_for, c_end_bac),False)
 
 # Find intersection point between center_circle and long edges of three rectangles
 circle_center = (0, 0)
 
 line_start   = (a_end_for.x, a_end_for.y)
 line_end     = (a_end_half.x, a_end_half.y)
-intersection = circle_line_segment_intersection(circle_center, center_radius, line_start, line_end)
+intersection = circle_line_segment_intersection(center_radius, line_start, line_end)
 a_int_for    = Vector(intersection[0][0], intersection[0][1], 0)
 a_for_inter  = Sketch_obj.addGeometry(Part.LineSegment(a_end_for, a_int_for),False)
 
 line_start   = (a_end_bac.x, a_end_bac.y)
 line_end     = (- a_end_half.x, - a_end_half.y)
-intersection = circle_line_segment_intersection(circle_center, center_radius, line_start, line_end)
+intersection = circle_line_segment_intersection(center_radius, line_start, line_end)
 a_int_bac    = Vector(intersection[0][0], intersection[0][1],0)
 a_bac_inter  = Sketch_obj.addGeometry(Part.LineSegment(a_end_bac, a_int_bac),False)
 
 
 line_end     = (b_end_for.x, b_end_for.y)
 line_start   = (b_end_half.x, b_end_half.y)
-intersection = circle_line_segment_intersection(circle_center, center_radius, line_start, line_end)
+intersection = circle_line_segment_intersection( center_radius, line_start, line_end)
 b_int_for    = Vector(intersection[0][0], intersection[0][1],0)
 b_for_inter  = Sketch_obj.addGeometry(Part.LineSegment(b_end_for, b_int_for),False)
 
 line_end     = (b_end_bac.x, b_end_bac.y)
 line_start   = (- b_end_half.x, - b_end_half.y)
-intersection = circle_line_segment_intersection(circle_center, center_radius, line_start, line_end)
+intersection = circle_line_segment_intersection(center_radius, line_start, line_end)
 b_int_bac    = Vector(intersection[0][0], intersection[0][1],0)
 b_bac_inter  = Sketch_obj.addGeometry(Part.LineSegment(b_end_bac, b_int_bac),False)
 
 
-line_start   = (c_end_for.x, c_end_for.y)
-line_end     = (c_end_half.x, c_end_half.y)
-intersection = circle_line_segment_intersection(circle_center, center_radius, line_start, line_end)
-c_int_for    = Vector(intersection[0][0], intersection[0][1],0)
-c_for_inter  = Sketch_obj.addGeometry(Part.LineSegment(c_end_for, c_int_for),False)
-
-line_start   = (c_end_bac.x, c_end_bac.y)
-line_end     = (- c_end_half.x, - c_end_half.y)
-intersection = circle_line_segment_intersection(circle_center, center_radius, line_start, line_end)
-c_int_bac    = Vector(intersection[0][0], intersection[0][1],0)
-c_bac_inter  = Sketch_obj.addGeometry(Part.LineSegment(c_end_bac, c_int_bac),False)
-
 # connect the arms 
 a_to_b = Sketch_obj.addGeometry(Part.LineSegment(a_int_for, b_int_bac),False)
-b_to_c = Sketch_obj.addGeometry(Part.LineSegment(b_int_for, c_int_bac),False)
-c_to_a = Sketch_obj.addGeometry(Part.LineSegment(c_int_for, a_int_bac),False)
+b_to_a = Sketch_obj.addGeometry(Part.LineSegment(b_int_for, a_int_bac),False)
 
 # pad 
 PadLabel  = 'Pad_main'
@@ -194,38 +169,30 @@ Sketch_topface.ViewObject.hide()
 # hole points of the end lines
 a_end_hole_for = a_end_for - polar_to_vector(cover_width, a_deg + 90) + polar_to_vector(0.1, a_deg)
 b_end_hole_for = b_end_for - polar_to_vector(cover_width, b_deg + 90) + polar_to_vector(0.1, b_deg)
-c_end_hole_for = c_end_for - polar_to_vector(cover_width, c_deg + 90) + polar_to_vector(0.1, c_deg)
 a_end_hole_bac = a_end_bac + polar_to_vector(cover_width, a_deg + 90) + polar_to_vector(0.1, a_deg)
 b_end_hole_bac = b_end_bac + polar_to_vector(cover_width, b_deg + 90) + polar_to_vector(0.1, b_deg)
-c_end_hole_bac = c_end_bac + polar_to_vector(cover_width, c_deg + 90) + polar_to_vector(0.1, c_deg)
 
 # hole points of the inner lines
 a_inner_hole_for =   polar_to_vector(hole_width/2, a_deg + 90) + polar_to_vector(center_radius, a_deg )
 b_inner_hole_for =   polar_to_vector(hole_width/2, b_deg + 90) + polar_to_vector(center_radius, b_deg )
-c_inner_hole_for =   polar_to_vector(hole_width/2, c_deg + 90) + polar_to_vector(center_radius, c_deg )
 a_inner_hole_bac = - polar_to_vector(hole_width/2, a_deg + 90) + polar_to_vector(center_radius, a_deg )
 b_inner_hole_bac = - polar_to_vector(hole_width/2, b_deg + 90) + polar_to_vector(center_radius, b_deg )
-c_inner_hole_bac = - polar_to_vector(hole_width/2, c_deg + 90) + polar_to_vector(center_radius, c_deg )
 
 # lines
 a_end_hole   = Sketch_topface.addGeometry(Part.LineSegment(a_end_hole_for, a_end_hole_bac),False)
 b_end_hole   = Sketch_topface.addGeometry(Part.LineSegment(b_end_hole_for, b_end_hole_bac),False)
-c_end_hole   = Sketch_topface.addGeometry(Part.LineSegment(c_end_hole_for, c_end_hole_bac),False)
 a_inner_hole = Sketch_topface.addGeometry(Part.LineSegment(a_inner_hole_for, a_inner_hole_bac),False)
 b_inner_hole = Sketch_topface.addGeometry(Part.LineSegment(b_inner_hole_for, b_inner_hole_bac),False)
-c_inner_hole = Sketch_topface.addGeometry(Part.LineSegment(c_inner_hole_for, c_inner_hole_bac),False)
 a_for_hole   = Sketch_topface.addGeometry(Part.LineSegment(a_end_hole_for, a_inner_hole_for),False)
 b_for_hole   = Sketch_topface.addGeometry(Part.LineSegment(b_end_hole_for, b_inner_hole_for),False)
-c_for_hole   = Sketch_topface.addGeometry(Part.LineSegment(c_end_hole_for, c_inner_hole_for),False)
 a_bac_hole   = Sketch_topface.addGeometry(Part.LineSegment(a_end_hole_bac, a_inner_hole_bac),False)
 b_bac_hole   = Sketch_topface.addGeometry(Part.LineSegment(b_end_hole_bac, b_inner_hole_bac),False)
-c_bac_hole   = Sketch_topface.addGeometry(Part.LineSegment(c_end_hole_bac, c_inner_hole_bac),False)
 
 # the hole
 PocketLabel = 'Pocket_hole'
 Pocket_hole = doc.getObject(BodyLabel).newObject('PartDesign::Pocket','Pocket')
 Pocket_hole.Profile = Sketch_topface
-Pocket_hole.Length = depth - 2
+Pocket_hole.Length = depth - gluepart_depth
 
 # bottom circle to glue to wall
 # sketch
@@ -260,7 +227,7 @@ Mesh_Glue.Label = Mesh_Glue_Label
 # 3mf
 export_list = []
 export_list.append(Mesh_Glue)
-Mesh.export(export_list, u"/home/paul/FreeCAD models/smurf/Glue_corner.3mf")
+Mesh.export(export_list, u"/home/paul/FreeCAD models/smurf/Glue_two_way.3mf")
 
 doc.recompute()
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
--- sides_and_shelves.py
+++ sides_and_shelves.py
@@ -1,5 +1,7 @@
 """
-sides_and_shelves.py -- Paul Cobbaut, 2024-05-14
+sides_and_shelves.py -- Paul Cobbaut
+2024-05-14
+2024-06-15 no longer going for inserts, instead gravity will do it
 Create a hexagon wall display for small figurines.
 3D-printed hexagon, covered by a bought plexiglass panel.
 This file ==> Sides and shelves
@@ -8,6 +10,8 @@ This file ==> Sides and shelves
 3. side
 4. side with hinges
 5. hinge leaf to hold plexiglass panel
+6. side with quartershelve
+7. side with double quartershelve
 """
 
 import FreeCAD
@@ -19,27 +23,47 @@ import Mesh
 import MeshPart
 import math
 
-#Dimensions in mm
-common_height = 42.00
-common_width  =  6.00
-insert_width_shelve  =  1.60 # shelves loosely in hexagon sides MK3 print
-insert_width_side    =  2.10 # sides firmly in hexagon corners MK4 print
-insert_length = 10.00 # insert in corner or side
-ridge_width  = 2
-ridge_height = 3
+# math
+cos30 = 0.866 # approximate cosine of 30 degree angle
+sin30 = 0.500 # exact
 
-short_length = 207  # short shelve
-long_length  = 288  # long shelve
+# dimensions are in mm
 
-side_length  = 130  # side of hexagon
+# sides and shelves
+common_height = 42
+common_width  =  6
 
+# shelves
+short_length = 211 # short shelve (was 207 with previous)
+long_length  = 288 # long shelve
+cross_length =  10 # where shelves rest on
+cross_cut    =   2 # no overlap at the back
+cross_height = common_height - cross_cut
+cross_width  = common_width/2
+
+# sides
+side_length   = 130  # side of hexagon
+insert_length =  10
+insert_gap    =   2
+insert_height = common_height - (2*insert_gap)
+insert_width  =   2.10 # sides firmly in hexagon corners MK4 print
+insert_Y      = (common_width - insert_width)/2
+ridge_width   =   2
+ridge_Y       = (common_width - ridge_width)/2
+ridge_height  =   3
+
+# holders
+holder_length = 20
+groove_length = 10
+
+# hinge
 hinge_length = 20
 hinge_outer  = 3.0  # radius
 hinge_inner  = 1.1  # radius
 
-hole_mm      = 1.5  # radius
-
+# leaf
 leaf_thickness = 3
+hole_mm      = 1.5  # radius
 
 # Create document
 doc = FreeCAD.newDocument("hexagon sides")
@@ -52,38 +76,145 @@ def makebox(label, length, width, height):
   obj.Height = height
   return obj
 
-# 1. short shelve --> main part and two insert parts
-short_shelve_main  = makebox('short_shelve_main' , short_length , common_width       , common_height)
-short_shelve_left  = makebox('short_shelve_left' , insert_length, insert_width_shelve, common_height)
-short_shelve_right = makebox('short_shelve_right', insert_length, insert_width_shelve, common_height)
-short_shelve_left.Placement  = FreeCAD.Placement(Vector(-insert_length, (common_width - insert_width_shelve)/2, 0),FreeCAD.Rotation(Vector(1,0,0),0))
-short_shelve_right.Placement = FreeCAD.Placement(Vector(short_length  , (common_width - insert_width_shelve)/2, 0),FreeCAD.Rotation(Vector(1,0,0),0))
+
+
+# 1. short shelve --> main part and two cross parts
+short_shelve_main  = makebox('short_shelve_main' , short_length, common_width, common_height)
+short_shelve_left  = makebox('short_shelve_left' , cross_length, cross_width , cross_height )
+short_shelve_right = makebox('short_shelve_right', cross_length, cross_width , cross_height )
+short_shelve_left.Placement  = FreeCAD.Placement(Vector(-cross_length, 0, 0),FreeCAD.Rotation(Vector(1,0,0),0))
+short_shelve_right.Placement = FreeCAD.Placement(Vector(short_length , 0, 0),FreeCAD.Rotation(Vector(1,0,0),0))
 short_shelve_compound        = doc.addObject("Part::Compound","short_shelve_compound")
 short_shelve_compound.Links  = [short_shelve_main, short_shelve_left, short_shelve_right,]
-#short_shelve_compound.ViewObject.hide()
+short_shelve_compound.ViewObject.hide()
 
-# 2. long shelve --> main part and two insert parts
-long_shelve_main  = makebox('long_shelve_main' , long_length  , common_width  , common_height)
-long_shelve_left  = makebox('long_shelve_left' , insert_length, common_width/2, common_height-2)
-long_shelve_right = makebox('long_shelve_right', insert_length, common_width/2, common_height-2)
-long_shelve_left.Placement  = FreeCAD.Placement(Vector(-insert_length, 0, 0),FreeCAD.Rotation(Vector(1,0,0),0))
-long_shelve_right.Placement = FreeCAD.Placement(Vector(long_length   , 0, 0),FreeCAD.Rotation(Vector(1,0,0),0))
+# 2. long shelve --> main part and two cross parts
+long_shelve_main  = makebox('long_shelve_main' , long_length , common_width, common_height)
+long_shelve_left  = makebox('long_shelve_left' , cross_length, cross_width , cross_height )
+long_shelve_right = makebox('long_shelve_right', cross_length, cross_width , cross_height )
+long_shelve_left.Placement  = FreeCAD.Placement(Vector(-cross_length, 0, 0),FreeCAD.Rotation(Vector(1,0,0),0))
+long_shelve_right.Placement = FreeCAD.Placement(Vector(long_length  , 0, 0),FreeCAD.Rotation(Vector(1,0,0),0))
 long_shelve_compound        = doc.addObject("Part::Compound","long_shelve_compound")
 long_shelve_compound.Links  = [long_shelve_main, long_shelve_left, long_shelve_right,]
-#long_shelve_compound.ViewObject.hide()
+long_shelve_compound.ViewObject.hide()
 
 # 3. side --> main part and two smaller insert parts and a ridge on top that holds the plexiglass
-side_main  = makebox('side_main' , side_length  , common_width     , common_height    )
-side_left  = makebox('side_left' , insert_length, insert_width_side, common_height - 4)
-side_right = makebox('side_right', insert_length, insert_width_side, common_height - 4)
-side_ridge = makebox('side_ridge', side_length  , ridge_width      , ridge_height     )
-side_left.Placement  = FreeCAD.Placement(Vector(-insert_length, (common_width - insert_width_side)/2, 2            ), FreeCAD.Rotation(Vector(1,0,0), 0))
-side_right.Placement = FreeCAD.Placement(Vector(side_length   , (common_width - insert_width_side)/2, 2            ), FreeCAD.Rotation(Vector(1,0,0), 0))
-side_ridge.Placement = FreeCAD.Placement(Vector(0             , (common_width - ridge_width)/2      , common_height), FreeCAD.Rotation(Vector(1,0,0), 0))
+side_main  = makebox('side_main' , side_length  , common_width, common_height)
+side_left  = makebox('side_left' , insert_length, insert_width, insert_height)
+side_right = makebox('side_right', insert_length, insert_width, insert_height)
+side_ridge = makebox('side_ridge', side_length  , ridge_width , ridge_height )
+side_left.Placement  = FreeCAD.Placement(Vector(-insert_length, insert_Y, 2            ), FreeCAD.Rotation(Vector(1,0,0), 0))
+side_right.Placement = FreeCAD.Placement(Vector(side_length   , insert_Y, 2            ), FreeCAD.Rotation(Vector(1,0,0), 0))
+side_ridge.Placement = FreeCAD.Placement(Vector(0             , ridge_Y , common_height), FreeCAD.Rotation(Vector(1,0,0), 0))
 side_compound        = doc.addObject("Part::Compound","side_compound")
 side_compound.Links  = [side_main, side_left, side_right,side_ridge]
-#side_compound.ViewObject.hide()
+side_compound.ViewObject.hide()
 
+
+# 6. side --> normal side with half-extension for quartershelve
+sideq_main  = makebox('sideq_main' , side_length  , common_width, common_height)
+sideq_left  = makebox('sideq_left' , insert_length, insert_width, insert_height)
+sideq_right = makebox('sideq_right', insert_length, insert_width, insert_height)
+sideq_ridge = makebox('sideq_ridge', side_length  , ridge_width , ridge_height )
+sideq_left.Placement  = FreeCAD.Placement(Vector(-insert_length, insert_Y, 2            ), FreeCAD.Rotation(Vector(1,0,0), 0))
+sideq_right.Placement = FreeCAD.Placement(Vector(side_length   , insert_Y, 2            ), FreeCAD.Rotation(Vector(1,0,0), 0))
+sideq_ridge.Placement = FreeCAD.Placement(Vector(0             , ridge_Y , common_height), FreeCAD.Rotation(Vector(1,0,0), 0))
+# holders for shelve on the side, midway
+# Pythagoras helps a bit, 0.866 = cos30
+hypo   = common_width / cos30
+# main for the solid part
+holder1_main = makebox('holder1_main', common_width  , holder_length/2, common_height)
+holder2_main = makebox('holder2_main', common_width  , holder_length/2, common_height)
+holder1_main.Placement = FreeCAD.Placement(Vector(side_length/2 - hypo/2, common_width/2, 0),App.Rotation(App.Vector(0,0,1),30))
+holder2_main.Placement = FreeCAD.Placement(Vector(side_length/2 + hypo/2, common_width/2, 0),App.Rotation(App.Vector(0,0,1),210))
+# extr for the extrusion on which the shelve rests
+holder1_extr = makebox('holder1_extr', common_width/2, holder_length/2, common_height)
+holder2_extr = makebox('holder2_extr', common_width/2, holder_length/2, common_height)
+X1 = (side_length / 2) - (hypo / 2) - (insert_length * sin30)
+X2 = (side_length / 2) + (hypo / 2) + (insert_length * sin30) - ((common_width / 2) * cos30)
+Y1 = (common_width / 2) + (insert_length * cos30)
+Y2 = (common_width / 2) - (insert_length * cos30) - ((common_width / 2) * sin30)
+holder1_extr.Placement = FreeCAD.Placement(Vector(X1, Y1, 0),App.Rotation(App.Vector(0,0,1),30))
+holder2_extr.Placement = FreeCAD.Placement(Vector(X2, Y2, 0),App.Rotation(App.Vector(0,0,1),210))
+# edge for the back to push the shelve against
+holder1_edge = makebox('holder1_edge', common_width/2, holder_length/2, cross_cut)
+holder2_edge = makebox('holder2_edge', common_width/2, holder_length/2, cross_cut )
+X3 = (side_length / 2) - (hypo / 2) - (insert_length * sin30) + ((common_width / 2) * cos30)
+X4 = (side_length / 2) + (hypo / 2) + (insert_length * sin30)
+Y3 = (common_width / 2) + (insert_length * cos30) + ((common_width / 2) * sin30)
+Y4 = (common_width / 2) - (insert_length * cos30)   
+holder1_edge.Placement = FreeCAD.Placement(Vector(X3, Y3, 0),App.Rotation(App.Vector(0,0,1),30))
+holder2_edge.Placement = FreeCAD.Placement(Vector(X4, Y4, 0),App.Rotation(App.Vector(0,0,1),210))
+
+sideq_compound        = doc.addObject("Part::Compound","sideq_compound")
+sideq_compound.Links  = [sideq_main, sideq_left, sideq_right,sideq_ridge, holder1_main, holder1_extr, holder1_edge, holder2_main, holder2_extr, holder2_edge]
+#sideq_compound.ViewObject.hide()
+
+# refine
+Refine_sideq = doc.addObject('Part::Refine','Refine_sideq')
+Refine_sideq.Source = sideq_compound
+Refine_sideq.Label = 'Refine_sideq'
+Refine_sideq.ViewObject.hide()
+doc.recompute()
+# mesh
+Mesh_sideq = doc.addObject("Mesh::Feature","Mesh_sideq")
+Shape = Part.getShape(Refine_sideq,"")
+Mesh_sideq.Mesh = MeshPart.meshFromShape(Shape=Shape, LinearDeflection=1, AngularDeflection=0.1, Relative=False)
+Mesh_sideq.Label = "Mesh_sideq"
+# 3mf
+export_list = []
+export_list.append(Mesh_sideq)
+Mesh.export(export_list, u"/home/paul/FreeCAD models/smurf/sideq.3mf")
+
+
+
+sideq2_compound        = doc.addObject("Part::Compound","sideq2_compound")
+sideq2_compound.Links  = [sideq_main, sideq_left, sideq_right,sideq_ridge, holder1_main, holder1_extr, holder1_edge]
+#sideq_compound.ViewObject.hide()
+
+# refine
+Refine_sideq2 = doc.addObject('Part::Refine','Refine_sideq2')
+Refine_sideq2.Source = sideq2_compound
+Refine_sideq2.Label = 'Refine_sideq2'
+Refine_sideq2.ViewObject.hide()
+doc.recompute()
+# mesh
+Mesh_sideq2 = doc.addObject("Mesh::Feature","Mesh_sideq2")
+Shape = Part.getShape(Refine_sideq2,"")
+Mesh_sideq2.Mesh = MeshPart.meshFromShape(Shape=Shape, LinearDeflection=1, AngularDeflection=0.1, Relative=False)
+Mesh_sideq2.Label = "Mesh_sideq2"
+# 3mf
+export_list = []
+export_list.append(Mesh_sideq2)
+Mesh.export(export_list, u"/home/paul/FreeCAD models/smurf/sideq2.3mf")
+
+
+
+sideq3_compound        = doc.addObject("Part::Compound","sideq3_compound")
+sideq3_compound.Links  = [sideq_main, sideq_left, sideq_right,sideq_ridge, holder2_main, holder2_extr, holder2_edge]
+#sideq_compound.ViewObject.hide()
+
+# refine
+Refine_sideq3 = doc.addObject('Part::Refine','Refine_sideq3')
+Refine_sideq3.Source = sideq3_compound
+Refine_sideq3.Label = 'Refine_sideq3'
+Refine_sideq3.ViewObject.hide()
+doc.recompute()
+# mesh
+Mesh_sideq3 = doc.addObject("Mesh::Feature","Mesh_sideq3")
+Shape = Part.getShape(Refine_sideq3,"")
+Mesh_sideq3.Mesh = MeshPart.meshFromShape(Shape=Shape, LinearDeflection=1, AngularDeflection=0.1, Relative=False)
+Mesh_sideq3.Label = "Mesh_sideq3"
+# 3mf
+export_list = []
+export_list.append(Mesh_sideq3)
+Mesh.export(export_list, u"/home/paul/FreeCAD models/smurf/sideq3.3mf")
+
+
+
+
+
+'''
 # 4. side with hinge --> identical to short side, plus two hinge parts
 side_hinge_main  = makebox('side_hinge_main' , side_length  , common_width     , common_height)
 side_hinge_left  = makebox('side_hinge_left' , insert_length, insert_width_side, common_height - 4)
@@ -94,6 +225,8 @@ side_hinge_left.Placement  = FreeCAD.Placement(Vector(-insert_length, (common_wi
 side_hinge_right.Placement = FreeCAD.Placement(Vector(side_length   , (common_width - insert_width_side)/2, 2            ), FreeCAD.Rotation(Vector(1,0,0), 0))
 side_hinge_ridg1.Placement = FreeCAD.Placement(Vector(0             , (common_width - ridge_width)/2      , common_height), FreeCAD.Rotation(Vector(1,0,0), 0))
 side_hinge_ridg2.Placement = FreeCAD.Placement(Vector(side_length/2 + hinge_length/2 +1 , (common_width - ridge_width)/2      , common_height), FreeCAD.Rotation(Vector(1,0,0), 0))
+side_hinge_ridg1.ViewObject.hide()
+side_hinge_ridg2.ViewObject.hide()
 # these two chamfers allow for wider opening of the plexiglass door
 # find ridge edges to chamfer
 for i, e in enumerate(side_hinge_ridg1.Shape.Edges): # Going through all edges of the left ridge
@@ -153,7 +286,7 @@ side_main_cut.Tool = hinge_cut
 # hinge compound
 side_hinge_compound        = doc.addObject("Part::Compound","side_hinge_compound")
 side_hinge_compound.Links  = [side_main_cut, side_hinge_left, side_hinge_right, chamfer_ridg1, chamfer_ridg2, hinge_right, hinge_left,]
-#side_hinge_compound.ViewObject.hide()
+side_hinge_compound.ViewObject.hide()
 
 # 5. hinge leaf
 # middle hinge is two tubes; outer and inner
@@ -178,6 +311,7 @@ extleaf = doc.addObject('Part::Extrusion','extleaf')
 extleaf.Base = hexleaf
 extleaf.LengthFwd = leaf_thickness
 extleaf.Solid = True
+hexleaf.ViewObject.hide()
 # remove overlap with side hinges from hexleaf
 over_left  = makebox('side_hinge_left' , 7, 4, 1)
 over_right = makebox('side_hinge_right', 7, 4, 1)
@@ -209,7 +343,9 @@ cuthole2.Tool = hole2
 # leaf compound
 leaf_compound        = doc.addObject("Part::Compound","leaf_compound")
 leaf_compound.Links  = [cuthole2, hinge_middle,]
-#leaf_compound.ViewObject.hide()
+leaf_compound.ViewObject.hide()
+
+'''
 
 doc.recompute()
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
